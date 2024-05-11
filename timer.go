7b1904e1f467525a155f1639a5b2bf7a6f7493bb
nexryai <nexryai@sda1.net>
2024-05-11 14:13:22 +0900

    Add files

--- timer.go
+++ timer.go
@@ -1,16 +1,52 @@
 package watchmaker
 
-import "time"
+import (
+	"time"
+)
 
 type Timer struct {
-	BaseInterval time.Time
-	SetOnTheHour bool
-	Delay        time.Time
-	// 型なんとかする
-	Timezone string
+	BaseInterval time.Duration
+	RunOnTheHour bool
+	Delay        time.Duration
+	Timezone     *time.Location
+	LastRun      time.Time
 }
 
 type BreakTime struct {
-	From time.Time
-	To   time.Time
+	From  time.Time
+	Until time.Time
+}
+
+func (t *Timer) WaitForNextScheduledTime() {
+	if t.Timezone == nil {
+		t.Timezone = time.Local
+	}
+
+	baseTime := time.Now().In(t.Timezone)
+
+	if t.RunOnTheHour {
+		// Truncate to the hour
+		// ex: 2019-01-01 12:34:56 -> 2019-01-01 12:00:00
+		baseTime = baseTime.Truncate(time.Hour)
+	}
+
+	nextTime := baseTime.Add(t.BaseInterval)
+	if t.Delay > 0 {
+		nextTime = nextTime.Add(t.Delay)
+	}
+
+	// もし次の時間が過去の時間だったら、baseIntervalを足していく
+	for {
+		if nextTime.Before(time.Now().In(t.Timezone)) {
+			nextTime = nextTime.Add(t.BaseInterval)
+		} else {
+			break
+		}
+	}
+
+	// Wait until the next scheduled time
+	time.Sleep(nextTime.Sub(time.Now().In(t.Timezone)))
+	t.LastRun = time.Now().In(t.Timezone)
+
+	return
 }
---
+++ timer_test.go
@@ -0,0 +1,71 @@
+package watchmaker
+
+import (
+	"math/rand"
+	"strings"
+	"testing"
+	"time"
+)
+
+func TestTimer(t *testing.T) {
+	tz, err := time.LoadLocation("Asia/Shanghai")
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	timer := Timer{
+		BaseInterval: 10 * time.Second,
+		RunOnTheHour: false,
+		Delay:        5 * time.Second,
+		Timezone:     tz,
+	}
+
+	startedAt := time.Now()
+	timer.WaitForNextScheduledTime()
+	finishedAt := time.Now()
+
+	if finishedAt.Sub(startedAt) < 15*time.Second {
+		t.Errorf("Timer didn't wait for the next scheduled time")
+	} else if finishedAt.Sub(startedAt) > 17*time.Second {
+		t.Errorf("Timer waited too long")
+	}
+}
+
+func testRunOnTheHour(t *testing.T) {
+	tz, err := time.LoadLocation("Asia/Shanghai")
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	timer := Timer{
+		BaseInterval: 10 * time.Second,
+		RunOnTheHour: true,
+		Delay:        10 * time.Second,
+		Timezone:     tz,
+	}
+
+	startedAt := time.Now()
+	timer.WaitForNextScheduledTime()
+	finishedAt := time.Now()
+
+	t.Logf("Started at: " + startedAt.Format("2006-01-02 15:04:05"))
+	t.Logf("Finished at: " + finishedAt.Format("2006-01-02 15:04:05"))
+
+	if !strings.HasSuffix(finishedAt.Format("2006-01-02 15:04:05"), "0") {
+		t.Errorf("Timer didn't run on the hour: " + finishedAt.Format("2006-01-02 15:04:05"))
+	} else {
+		t.Logf("Timer ran on the hour: " + finishedAt.Format("2006-01-02 15:04:05"))
+	}
+
+	if finishedAt.Sub(startedAt) > 10*time.Second {
+		t.Errorf("Timer waited too long")
+	}
+}
+
+func TestOnTheHourManyTimes(t *testing.T) {
+	for i := 0; i < 3; i++ {
+		testRunOnTheHour(t)
+		// 10-20秒ランダム待機
+		time.Sleep(time.Duration(10+rand.Intn(10)) * time.Second)
+	}
+}
